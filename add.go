44a7d49b5a4b2c3a3cbe864a846c4829cc23caa2
Emanuele Rocco Petrone <dev@emaele.xyz>
2021-02-14 01:08:13 +0100

    better error flow

--- add.go
+++ add.go
@@ -15,15 +15,21 @@ func addFeed(writer http.ResponseWriter, request *http.Request) {
 		}
 	}()
 
+	// reading body as bytes stream
 	bodyBytes, err := ioutil.ReadAll(request.Body)
 	if err != nil {
 		log.Panic(err)
+		writer.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 	bodyString := string(bodyBytes)
 
+	// parsing url from body
 	feed, err := feedParser.ParseURL(bodyString)
 	if err != nil {
 		log.Panic(err)
+		writer.WriteHeader(http.StatusUnprocessableEntity)
+		return
 	}
 
 	rssFeed := rssfeed{
@@ -34,13 +40,13 @@ func addFeed(writer http.ResponseWriter, request *http.Request) {
 
 	var f rssfeed
 	rows := db.Where(rssfeed{URL: rssFeed.URL}).Find(&f).RowsAffected
-	if rows != 0 {
+	if rows != 0 { // if there are rows affected we have a duplicate
 		writer.WriteHeader(http.StatusUnprocessableEntity)
 		writer.Write([]byte("duplicate!"))
-
 		return
 	}
 
+	// Adding feed to db
 	err = db.Create(&rssFeed).Error
 	if err != nil {
 		writer.WriteHeader(http.StatusInternalServerError)
