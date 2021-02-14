b62b1e8d4c3b48d779843eba97064707de12a057
Emanuele Rocco Petrone <dev@emaele.xyz>
2021-02-14 12:48:53 +0100

    delete feed

---
+++ delete.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func deleteFeed(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+
+	feed, ok := vars["id"]
+	if !ok {
+		writer.WriteHeader(http.StatusNotFound)
+		writer.Write([]byte("request feed is not found"))
+		return
+	}
+
+	var f rssFeed
+
+	rows := db.Where("ID = ?", feed).Find(&f).RowsAffected
+
+	if rows == 0 {
+		writer.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	err := db.Delete(&f).Error
+	if err != nil {
+		log.Panic(err)
+		writeHTTPResponse(http.StatusInternalServerError, "unable to delete", writer)
+	}
+
+	return
+}
