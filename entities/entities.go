92e6550f607d1f032eb85854889ae668a6541866
Emanuele Rocco Petrone <dev@emaele.xyz>
2021-02-16 14:07:53 +0100

    update entities

--- entities/entities.go
+++ entities/entities.go
@@ -2,26 +2,35 @@ package entities
 
 import (
 	"time"
+
+	"gorm.io/gorm"
 )
 
+// RssItem is a rss element
 type RssItem struct {
-	ID          uint      `json:"-" gorm:"primarykey"`
-	CreatedAt   time.Time `json:"-"`
-	Title       string    `json:"Title"`
-	Description string    `json:"Description"`
-	URL         string    `json:"URL" gorm:"not null,unique"`
-	Sent        bool      `json:"-"`
-	Feed        uint      `json:"-" gorm:"not null"`
+	ID          uint           `json:"-" gorm:"primarykey"`
+	CreatedAt   time.Time      `json:"-"`
+	UpdatedAt   time.Time      `json:"-"`
+	DeletedAt   gorm.DeletedAt `gorm:"index"`
+	Title       string         `json:"Title"`
+	Description string         `json:"Description"`
+	URL         string         `json:"URL" gorm:"not null,unique"`
+	Sent        bool           `json:"-"`
+	Feed        uint           `json:"-" gorm:"not null"`
 }
 
+// RssFeed represents a rss feed
 type RssFeed struct {
-	ID          uint      `json:"ID" gorm:"primarykey"`
-	CreatedAt   time.Time `json:"-"`
-	Title       string    `json:"Title"`
-	Description string    `json:"Description"`
-	URL         string    `json:"URL" gorm:"not null,unique"`
+	ID          uint           `json:"ID" gorm:"primarykey"`
+	CreatedAt   time.Time      `json:"-"`
+	UpdatedAt   time.Time      `json:"-"`
+	DeletedAt   gorm.DeletedAt `gorm:"index"`
+	Title       string         `json:"Title"`
+	Description string         `json:"Description"`
+	URL         string         `json:"URL" gorm:"not null,unique"`
 }
 
+// AddFeedRequest is the struct for the add request
 type AddFeedRequest struct {
 	URL string `json:"URL"`
 }
