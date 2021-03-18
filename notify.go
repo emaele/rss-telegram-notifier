2bfdebd08c9de91bfaf20a43373d6fd059fe773e
Emanuele Rocco Petrone <dev@emaele.xyz>
2021-03-18 21:17:43 +0100

    wrap some telegram stuff

--- notify.go
+++ notify.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"time"
 
 	"github.com/emaele/rss-telegram-notifier/entities"
-	tb "gopkg.in/tucnak/telebot.v2"
 )
 
 func notificationRoutine() {
 
-	for range time.NewTicker(10 * time.Minute).C {
+	for range time.NewTicker(10 * time.Second).C {
 		var elements []entities.RssItem
 		rows := db.Where("sent = ?", false).Find(&elements).RowsAffected
 
@@ -20,9 +18,10 @@ func notificationRoutine() {
 		}
 
 		for _, element := range elements {
-			message := fmt.Sprintf("%s \n %s", element.Title, element.URL)
 
-			_, err := bot.Send(&tb.Chat{ID: telegramChatID}, message)
+			message := createTelegramMessage(element)
+
+			_, err := bot.Send(message)
 			if err != nil {
 				log.Println(err)
 				continue
--- utility.go
+++ utility.go
@@ -1,10 +1,48 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/emaele/rss-telegram-notifier/entities"
+
+	tg "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
 
 func writeHTTPResponse(statusCode int, body string, writer http.ResponseWriter) {
 
 	writer.WriteHeader(statusCode)
-	writer.Write([]byte(body))
+	_, err := writer.Write([]byte(body))
+	if err != nil {
+		log.Println(err)
+	}
+
 	return
+}
+
+func createTelegramKeyboard(URL string) tg.InlineKeyboardMarkup {
+	var keyboard tg.InlineKeyboardMarkup
+	row := tg.NewInlineKeyboardRow(tg.NewInlineKeyboardButtonURL("🔗 Link", URL))
+	keyboard.InlineKeyboard = append(keyboard.InlineKeyboard, row)
+
+	//We finally append the lower row to the keyboard
+	keyboard.InlineKeyboard = append(keyboard.InlineKeyboard)
+
+	return keyboard
+}
+
+func createTelegramMessage(element entities.RssItem) tg.MessageConfig {
+
+	feedTitle := retrieveFeedTitle(element.Feed)
+
+	text := fmt.Sprintf("*%s*\n\n%s", feedTitle, element.Title)
+	text = strings.ReplaceAll(text, ".", "\\.")
+
+	message := tg.NewMessage(telegramChatID, text)
+	message.ParseMode = "MarkdownV2"
+	message.ReplyMarkup = createTelegramKeyboard(element.URL)
+
+	return message
 }
